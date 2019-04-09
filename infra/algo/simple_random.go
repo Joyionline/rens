31e6a593494e1008a6aca8247972d753d2610ae7
Joyionline <hhaz1dyy888@vip.qq.com>
2019-04-09 21:47:00 +0800

    Add algo

---
+++ infra/algo/simple_random.go
@@ -0,0 +1,30 @@
+package algo
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	min int64 = 1
+)
+
+/*
+	可能遇到的问题：
+	1. 每次生成的随机数都一样，
+		原因：需要设置种子数，如时间戳
+
+
+	该版本的问题：
+		生成的金额数是从大到小，越往后越小
+*/
+
+// 简单随机算法
+// 红包的数量，红包金额  单位：分  类型: int64
+func SimpleRand(count, amount int64) int64 {
+	// 最大可调度金额 = 红包总金额-最小的分配金额
+	max := amount - min*count
+	rand.Seed(time.Now().UnixNano()) // 设置种子数
+	x := rand.Int63n(max) + min
+	return x
+}
